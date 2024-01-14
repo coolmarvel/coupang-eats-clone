36028ddf8a26a421a734e178ea5c8b1ca5fbde71
coolmarvel <marvel97@naver.com>
2024-01-14 15:12:33 +0900

    backend menu order review store user module & controller generate

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImageModule } from './image/image.module';
+import { MenuModule } from './menu/menu.module';
+import { OrderModule } from './order/order.module';
+import { ReviewModule } from './review/review.module';
+import { StoreModule } from './store/store.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ImageModule, MenuModule, OrderModule, ReviewModule, StoreModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
