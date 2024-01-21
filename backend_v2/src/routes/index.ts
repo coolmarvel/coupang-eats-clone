499eb87592aa88acd0a26e50aa90999f0bd52203
coolmarvel <marvel97@naver.com>
2024-01-21 11:42:48 +0900

    store router create

--- backend_v2/src/routes/index.ts
+++ backend_v2/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import userRouter from './user';
 import orderRouter from './order';
+import storeRouter from './store';
 import reviewRouter from './review';
 
 const router = Router();
@@ -17,6 +18,7 @@ router.get('/', (req, res) => {
 
 router.use('/user', userRouter);
 router.use('/order', orderRouter);
+router.use('/store', storeRouter);
 router.use('/review', reviewRouter);
 
 export default router;
