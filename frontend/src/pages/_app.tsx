cd312a95cd617a4be22d199375c0ae18d5603895
coolmarvel <marvel97@naver.com>
2024-01-25 13:21:02 +0900

    global config edit and apply

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import { QueryClient } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 1000 * 60 * 5, // 5 minutes
+          cacheTime: Infinity,
+          refetchOnWindowFocus: false,
+          refetchOnMount: false,
+        },
+      },
+    })
+  );
+
   return <Component {...pageProps} />;
 }
--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,11 @@
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import dotenv from 'dotenv'
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import mongoClient from '@/libs/mongo-client';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// import mongoClient from '@/lib/mongoClient'
-dotenv.config()
-
-const providers = []
+const providers = [];
 
 if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   providers.push(
@@ -14,14 +13,14 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     })
-  )
+  );
 }
 
-// export const authOptions: NextAuthOptions = {
-//   providers,
-//   adapter: MongoDBAdapter(mongoClient),
-//   secret: process.env.NEXTAUTH_SECRET,
-//   debug: true,
-// }
+export const authOptions: NextAuthOptions = {
+  providers,
+  adapter: MongoDBAdapter(mongoClient),
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
+};
 
-// export default NextAuth(authOptions)
+export default NextAuth(authOptions);
