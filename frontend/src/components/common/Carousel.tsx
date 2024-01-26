1d9dbd25b9542cb3aad496891fff637bf4400fd9
coolmarvel <marvel97@naver.com>
2024-01-26 14:50:03 +0900

    public/imgs & package version downgrade

--- frontend/src/components/common/Carousel.tsx
+++ frontend/src/components/common/Carousel.tsx
@@ -4,13 +4,20 @@ import 'swiper/css';
 
 export const Carousel = ({ images, height = 'h-72' }: { images?: string[]; height?: string }) => {
   if (!images?.length)
-    return <div className={`${height} w-screen bg-cover bg-center bg-no-repeat`} style={{ backgroundImage: `url(/imgs/noimage.svg)` }}></div>;
+    return (
+      <div
+        className={`${height} w-screen bg-cover bg-center bg-no-repeat`}
+        style={{
+          backgroundImage: `url(/imgs/noimage.svg)`,
+        }}
+      ></div>
+    );
 
   return (
     <Swiper>
       {images?.map((image, index) => (
         <SwiperSlide key={`${image}_${index}`}>
-          <div className={`${height} w-screen bg-cover bg-center`} style={{ backgroundImage: `url(${image})` }}></div>
+          <div className={`${height} w-screen bg-cover bg-center`} style={{ backgroundImage: `url(${image})` }} />
         </SwiperSlide>
       ))}
     </Swiper>
